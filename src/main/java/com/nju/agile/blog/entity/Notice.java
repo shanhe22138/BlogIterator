d5ee44f1561fa30dce976103b99970b5d960b681
2451125842 <2451125842@qq.com>
2020-02-16 17:46:10 +0800

    dao update

--- src/main/java/com/nju/agile/blog/entity/Notice.java
+++ src/main/java/com/nju/agile/blog/entity/Notice.java
@@ -1,25 +1,17 @@
 package com.nju.agile.blog.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Date;
 
 /**
- * @className: Notice
- * @description: <p>页面顶部广播信息实体类</p>
- * @author: shanhe
- * @date: 2020-02-09 21:57
- * @version: 1.0
- **/
+ * @author liuyanzhao
+ */
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Notice implements Serializable {
-    private static final long serialVersionUID = -5613004046056060896L;
+public class Notice implements Serializable{
 
+    private static final long serialVersionUID = -4901560494243593100L;
     private Integer noticeId;
 
     private String noticeTitle;
